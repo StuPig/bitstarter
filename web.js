f54a6827478a93a29b564d3c5b57bc3c81b4ba96
StuPig <shouqiang.gong@gmail.com>
2013-07-10 21:04:10 +0800

    add index.html

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var express = express.createServer(express.logger());
+
+var app = express();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    fs.createReadStream(__dirname + '/index.html')
+        .once('open', function() {
+            response.writeHead(200, {'Content-type': 'text/plain'});
+            this.pipe(response);
+        })
+        .once('error', function() {
+            console.log(e);
+            response.writeHead(500);
+            response.end('Server Error');
+        });
 });
 
 var port = process.env.PORT || 5000;
